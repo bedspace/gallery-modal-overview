e51542bee892d4c4aeaa3dfb9bc0f57e577384c0
Julio Fuentes <juliofuentes408@gmail.com>
2019-07-11 18:28:25 -0700

    30% complete with positioning arrows and image in carousel

--- client/src/components/FullScreenModal.jsx
+++ client/src/components/FullScreenModal.jsx
@@ -19,7 +19,19 @@ class FullScreenModal extends React.Component {
                 <div className={showOrHide}>
                     <div style={{display: 'flex'}}>
                         <div id="carousel">
-                            <div className="carouselArrows"></div>
+                            <div className="carouselArrows">
+                                <button className="leftArrow">
+                                    <img src="https://bit.ly/2Y3gUUo"/>
+                                </button>
+                                <button className="rightArrow">
+                                    <img src="https://bit.ly/2Lj65ri"/>
+                                </button>
+                            </div>
+                            <span>
+                                <div>
+                                    <img className="carouselImg" src="https://bit.ly/2S8xQ6H"/>
+                                </div>
+                            </span>
                         </div>
                         <div id="sidePanel"></div>
                     </div>
